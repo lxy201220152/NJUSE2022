a62b70d2515572c1e253878ef24b4044f5a4f785
lxy201220152 <2269639044@qq.com>
2022-11-05 19:11:46 +0800

    branch dev2

--- main.cpp
+++ main.cpp
@@ -66,7 +66,8 @@ int main()
 		{
 			if(strcmp(ptr2->d_name, ".")!=0 && strcmp(ptr2->d_name,"..")!=0 && 
 			strcmp(ptr2->d_name, "stdin_format.txt")!=0 && strcmp(ptr2->d_name, "out1.txt")!=0
-			&& strcmp(ptr2->d_name, "in.txt")!=0 && strcmp(ptr2->d_name, "out2.txt")!=0
+			&& strcmp(ptr2->d_name, "in1.txt")!=0 && strcmp(ptr2->d_name, "in2.txt")!=0
+			&& strcmp(ptr2->d_name, "out2.txt")!=0
 			&& strcmp(ptr2->d_name, "a.out")!=0 && strcmp(ptr2->d_name, "b.out")!=0)
 			{
 				files.push_back(dirs[i] + "/" + ptr2->d_name);
@@ -74,8 +75,110 @@ int main()
 		}
 		closedir(pfile);
 
-		for(int j=0;j<files.size();j++)
-			cout<<files[j]<<endl;
+		//for(int j=0;j<files.size();j++)
+			//cout<<files[j]<<endl;
+
+		string sourcestr = "qwertyuioplkjhgfdsazxcvbnm";
+
+		string informpath = dirs[i] + "/stdin_format.txt";
+		fstream formf;
+		formf.open(informpath, ios::in);
+		if(!formf) cout<<"error"<<endl;
+		
+		vector<string> formstr;
+		while(!formf.eof())
+		{
+			string tmpstr;
+			formf >> tmpstr;
+			formstr.push_back(tmpstr);
+		}
+		formf.close();
+
+		fstream inf1;
+		string inpath1 = dirs[i] + "/in1.txt";
+		inf1.open(inpath1,ios::out);
+		if(!inf1) cout<<"error"<<endl;
+
+		for(int j=0;j<formstr.size();j++)
+		{
+			//cout<<formstr[j]<<endl;
+			if(formstr[j]=="char")
+			{	
+				char ch = rand()%26+'a';
+				inf1 << ch << " ";
+			}
+			else
+			{
+				string::size_type posit1, posit2, posit3;
+				posit1 = formstr[j].find_first_of("(");
+				posit2 = formstr[j].find_first_of(",");
+				posit3 = formstr[j].find_first_of(")");
+
+				string type = formstr[j].substr(0,posit1);
+				
+				if(type=="int")
+				{
+					int range1 = stoi(formstr[j].substr(posit1+1, posit2));
+					int range2 = stoi(formstr[j].substr(posit2+1, posit3));
+
+					inf1 << range1 + rand()%(range2-range1+1) << " ";
+				}
+				else if(type == "string")
+				{
+					int range1 = stoi(formstr[j].substr(posit1+1, posit2));
+					int range2 = stoi(formstr[j].substr(posit2+1, posit3));
+
+					int len = range1 + rand()%(range2-range1 + 1);
+					inf1 << sourcestr.substr(0,len) << " ";
+				}	
+			}
+		}
+		inf1.close();
+
+		fstream inf2;
+		string inpath2 = dirs[i] + "/in2.txt";
+		inf2.open(inpath2,ios::out);
+		if(!inf2) cout<<"error"<<endl;
+
+		for(int j=0;j<formstr.size();j++)
+		{
+			//cout<<formstr[j]<<endl;
+			if(formstr[j]=="char")
+			{	
+				char ch = rand()%26+'a';
+				inf2 << ch << " ";
+			}
+			else
+			{
+				string::size_type posit1, posit2, posit3;
+				posit1 = formstr[j].find_first_of("(");
+				posit2 = formstr[j].find_first_of(",");
+				posit3 = formstr[j].find_first_of(")");
+
+				string type = formstr[j].substr(0,posit1);
+				
+				if(type=="int")
+				{
+					int range1 = stoi(formstr[j].substr(posit1+1, posit2));
+					int range2 = stoi(formstr[j].substr(posit2+1, posit3));
+
+					inf2 << range1 + rand()%(range2-range1+1) << " ";
+				}
+				else if(type == "string")
+				{
+					int range1 = stoi(formstr[j].substr(posit1+1, posit2));
+					int range2 = stoi(formstr[j].substr(posit2+1, posit3));
+
+					int len = range1 + rand()%(range2-range1 + 1);
+					inf2 << sourcestr.substr(0,len) << " ";
+				}	
+			}
+		}
+		inf2.close();
+
+
+
+
 	}
 
 	return 0;
